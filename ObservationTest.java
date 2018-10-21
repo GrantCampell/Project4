dae2ebf2cd34d1b7975fcebbf140369e7e9339af
Grant Campbell <Grant.D.Campbell-1@ou.edu>
2018-10-21 12:41:53 -0500

    Create unit test classes

---
+++ ObservationTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ObservationTest
+{
+
+    @Test
+    public void testObservation()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetValue()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsValid()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetStid()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString()
+    {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ StatisticsTest.java
@@ -0,0 +1,68 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StatisticsTest
+{
+
+    @Test
+    public void testStatisticsDoubleStringStringIntStatsType()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testStatisticsDoubleStringGregorianCalendarIntStatsType()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCreateDateFromString()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCreateStringFromDate()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetNumberOfReportingStations()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetUTCDateTime()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testNewerThan()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testOlderThan()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSameAs()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString()
+    {
+        fail("Not yet implemented");
+    }
+
+}
