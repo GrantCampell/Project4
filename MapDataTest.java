6ea9593b9029ed60cedede0f3d0b43ce7682da83
Grant Campbell <Grant.D.Campbell-1@ou.edu>
2018-10-21 12:50:59 -0500

    Fixed MapDataTest

--- MapDataTest.java
+++ MapDataTest.java
@@ -6,7 +6,85 @@ public class MapDataTest
 {
 
     @Test
-    public void test()
+    public void testMapData()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCreateFileName()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testParseFile()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetTairMin()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetTairMax()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetTairAverage()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetTa9mMin()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetTa9mMax()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetTa9mAverage()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetSradMin()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetSradMax()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetSradAverage()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetSradTotal()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString()
     {
         fail("Not yet implemented");
     }
