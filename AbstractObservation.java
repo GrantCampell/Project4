a36c52baba6a69b606517804a12791eb165a8e6c
Grant Campbell <Grant.D.Campbell-1@ou.edu>
2018-10-22 20:31:00 -0500

    Added methods to AbsObs from UML

--- AbstractObservation.java
+++ AbstractObservation.java
@@ -1,5 +1,12 @@
 
 public abstract class AbstractObservation
 {
-
+    protected boolean valid;
+    
+    public abstract AbstractObservation();
+    
+    public boolean isValid()
+    {
+        //TODO: Complete this method
+    }
 }
