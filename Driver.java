04ccf92db46fb03996b8599b23abbff5d4158f1b
Grant Campbell <Grant.D.Campbell-1@ou.edu>
2018-10-28 00:56:01 -0500

    Driver class populated

--- Driver.java
+++ Driver.java
@@ -1,10 +1,23 @@
+import java.io.IOException;
 
 public class Driver
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
         // TODO Auto-generated method stub
+        final int YEAR = 2018;
+        final int MONTH = 8;
+        final int DAY = 1;
+        final int HOUR = 7;
+        final int MINUTE = 00;
+        final String directory = "data";
+
+        MapData mapData = new MapData(YEAR, MONTH, DAY, HOUR, MINUTE, directory);
+
+        mapData.parseFile();
+
+        System.out.println(mapData);
 
     }
 
