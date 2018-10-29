bbc5ca0e89f57154458ae97de832c4e36a8c70d2
Grant Campbell <Grant.D.Campbell-1@ou.edu>
2018-10-29 00:57:35 -0500

    Javadocs for driver

--- Driver.java
+++ Driver.java
@@ -1,22 +1,61 @@
 import java.io.IOException;
 
+/**
+ * 
+ * @author Grant Campbell
+ * @version 10/29/2018
+ * 
+ *          Driver for the program. Takes in date and time to have data found
+ *          for. Prints out the resulting statistics.
+ */
 public class Driver
 {
 
     public static void main(String[] args) throws IOException
     {
-        // TODO Auto-generated method stub
+        /**
+         * The year to be tested
+         */
         final int YEAR = 2018;
+
+        /**
+         * The month to be tested
+         */
         final int MONTH = 8;
+
+        /**
+         * The day to be tested
+         */
         final int DAY = 1;
+
+        /**
+         * The hour to be tested
+         */
         final int HOUR = 7;
+
+        /**
+         * The minute to be tested
+         */
         final int MINUTE = 00;
+
+        /**
+         * The directory the data is found in
+         */
         final String directory = "data";
 
+        /**
+         * Creates the MapData for the data
+         */
         MapData mapData = new MapData(YEAR, MONTH, DAY, HOUR, MINUTE, directory);
 
+        /**
+         * Entry point of the program. Pushes the rest of the instructions.
+         */
         mapData.parseFile();
 
+        /**
+         * Prints out the data summary.
+         */
         System.out.println(mapData);
 
     }
