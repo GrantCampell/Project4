6479fb1f01eb88aaf37a4bec6b7c6b3a3902c5c0
Grant Campbell <Grant.D.Campbell-1@ou.edu>
2018-10-29 00:43:08 -0500

    Javadoc added for AbstractObservation

--- AbstractObservation.java
+++ AbstractObservation.java
@@ -1,13 +1,32 @@
-
+/**
+ * 
+ * @author Grant Campbell
+ * @version 10/29/2018
+ * 
+ *          AbstractObservation is used to hold information taken in from the
+ *          data.
+ */
 public abstract class AbstractObservation
 {
+    /**
+     * Stores whether or not the Observation contains valid data.
+     */
     protected boolean valid;
 
+    /**
+     * Constructor for AbstractObservation. Checks the Observation class to find
+     * Observation validity.
+     */
     public AbstractObservation()
     {
         isValid();
     }
 
+    /**
+     * Works with the Observation class to decide if an Observation is valid.
+     * 
+     * @return Validity of the Observation.
+     */
     public boolean isValid()
     {
         return valid;
