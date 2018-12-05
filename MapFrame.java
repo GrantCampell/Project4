b6cd3c27470b3a8776503c37dbbbf5a908d86b9b
Grant Campbell <Grant.D.Campbell-1@ou.edu>
2018-12-05 15:19:50 -0600

    Last version before moving code over to new classes

--- MapFrame.java
+++ MapFrame.java
@@ -1,13 +1,9 @@
-import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
