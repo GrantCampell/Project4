5464f63ba2f59130a092082757be51944c944e34
Grant Campbell <Grant.D.Campbell-1@ou.edu>
2018-12-05 14:35:33 -0600

    Stats panel moved to new class

--- StatisticsPanel.java
+++ StatisticsPanel.java
@@ -1,5 +1,12 @@
+import javax.swing.ButtonGroup;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
-public class StatisticsPanel
+public class StatisticsPanel extends JPanel
 {
+    ButtonGroup stats = new ButtonGroup();
 
+    JRadioButton max = new JRadioButton("MAXIMUM");
+    JRadioButton min = new JRadioButton("MINIMUM");
+    JRadioButton avg = new JRadioButton("AVERAGE");
 }
