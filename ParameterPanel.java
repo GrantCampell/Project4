d54e84e9dc223d24b8c71e715994c03776e24341
Grant Campbell <Grant.D.Campbell-1@ou.edu>
2018-12-05 14:38:55 -0600

    Parameter panel moved to new class

--- ParameterPanel.java
+++ ParameterPanel.java
@@ -1,5 +1,15 @@
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
 
-public class ParameterPanel
+public class ParameterPanel extends JPanel
 {
-
+    /**
+     * Default ID
+     */
+    private static final long serialVersionUID = 1L;
+    JCheckBox tair = new JCheckBox("TAIR");
+    JCheckBox ta9m = new JCheckBox("TA9M");
+    JCheckBox srad = new JCheckBox("SRAD");
+    JCheckBox wspd = new JCheckBox("WSPD");
+    JCheckBox pres = new JCheckBox("PRES");
 }
