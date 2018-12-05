2a3fb205e129099bcc9f73851f7de2395d663bda
Grant Campbell <Grant.D.Campbell-1@ou.edu>
2018-12-05 14:42:39 -0600

    Tabel Panel moved to new class

--- TabelPanel.java
+++ TabelPanel.java
@@ -1,5 +1,16 @@
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
-public class TabelPanel
+public class TabelPanel extends JPanel
 {
+    /**
+     * Default ID
+     */
+    private static final long serialVersionUID = 1L;
+    String[] columnNames = { "Station", "Parameter", "Statistics", "Value", "Reporting Stations", "Date" };
+    Object[][] rowData = {};
 
+    private DefaultTableModel tableModel = new DefaultTableModel(rowData, columnNames);
+    private JTable table = new JTable(tableModel);
 }
