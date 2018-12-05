2891d47957d1aeccc7c593c255c55ee1524e5713
Grant Campbell <Grant.D.Campbell-1@ou.edu>
2018-12-05 14:28:46 -0600

    MenuBar moved to MesonetFrame class

--- MesonetFrame.java
+++ MesonetFrame.java
@@ -1,5 +1,20 @@
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-public class MesonetFrame
+public class MesonetFrame extends JFrame
 {
-
+    public class FileMenuBar extends JMenuBar
+    {
+        /**
+         * Default ID
+         */
+        private static final long serialVersionUID = 1L;
+         
+        JMenuBar menu = new JMenuBar();
+        JMenu file = new JMenu("File");
+        JMenuItem load = new JMenuItem("Open Data File");
+        JMenuItem exitFile = new JMenuItem("Exit");
+    }
 }
